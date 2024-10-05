7030d388f10a297d08039375321d575fc973c27c
Xuresch <niko.rauner@googlemail.com>
2024-10-05 12:24:09 +0200

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,6 +15,7 @@ export default function Index() {
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
             Welcome to <span className="sr-only">Remix</span>
           </h1>
+          <p>This is a test!</p>
           <div className="h-[144px] w-[434px]">
             <img
               src="/logo-light.png"
